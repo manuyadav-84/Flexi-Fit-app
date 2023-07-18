f6d5a3216075f97fe6be52c3399bf4d2f44f6574
Anil Shinde <anishind1@WKMZTE91E0F9.global.publicisgroupe.net>
2023-07-18 13:12:50 +0530

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,255 @@
+console.log(userData)
+
+
+// const userData = {
+//     "bucket": [
+//         {
+//         "startTimeMillis": "1687199400000", 
+//         "endTimeMillis": "1687285800000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687228630939792498", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687279425303100300", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 8483
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687285800000", 
+//         "endTimeMillis": "1687372200000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687310690303442029", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687369400412167352", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 5963
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687372200000", 
+//         "endTimeMillis": "1687458600000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687389041529581332", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687454393063772881", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 960
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687458600000", 
+//         "endTimeMillis": "1687545000000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687484069676096434", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687541534903187615", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 4002
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687545000000", 
+//         "endTimeMillis": "1687631400000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687573535238934901", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687627365890531034", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 2315
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687631400000", 
+//         "endTimeMillis": "1687717800000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687664159810774760", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687710867839893121", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 1265
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687717800000", 
+//         "endTimeMillis": "1687804200000", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687748442033862742", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687803622402132467", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 8385
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }, 
+//         {
+//         "startTimeMillis": "1687804200000", 
+//         "endTimeMillis": "1687852076183", 
+//         "dataset": [
+//             {
+//             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:aggregated", 
+//             "point": [
+//                 {
+//                 "startTimeNanos": "1687830664775325152", 
+//                 "originDataSourceId": "raw:com.google.step_count.cumulative:samsung:SM-G770F:d1754c9c:step_counter  Non-wakeup", 
+//                 "endTimeNanos": "1687848936318254015", 
+//                 "value": [
+//                     {
+//                     "mapVal": [], 
+//                     "intVal": 317
+//                     }
+//                 ], 
+//                 "dataTypeName": "com.google.step_count.delta"
+//                 }
+//             ]
+//             }
+//         ]
+//         }
+//     ]
+// }
+
+const data = userData.bucket;
+const lastData = data.length
+
+console.log(lastData);
+
+const selectData = 4;
+
+let startTimeMillis = +(data[lastData - selectData].startTimeMillis);
+let endTimeMillis = +(data[lastData - selectData].endTimeMillis);
+let startTimenanosecond = +(data[lastData - selectData].dataset[0].point[0].startTimeNanos);
+let endTimenanosecond = +(data[lastData - selectData].dataset[0].point[0].endTimeNanos);
+
+let userSteps = data[lastData - selectData].dataset[0].point[0].value[0].intVal;
+let distanceInKm = (data[lastData - selectData].dataset[2].point[0].value[0].fpVal / 1000).toFixed(2);
+let calories = Math.round(data[lastData - selectData].dataset[2].point[0].value[0].fpVal);
+
+console.log(distanceInKm, calories)
+
+let convertMilliSec = (x) => {
+    const nanoToMilliSec = +(String(Math.ceil(x / 1000000)))
+    function milliSecToDate(val) {
+        let date = new Date(val);
+        return date;
+    }
+    return milliSecToDate(nanoToMilliSec)
+};
+
+const milliSecToDate = (val) => {
+    let date = new Date(val);
+    return date;
+}
+
+const msToTime = (ms) => {
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    if (seconds < 60) return seconds + " Sec";
+    else if (minutes < 60) return minutes + " Min";
+    else if (hours < 24) return hours + " Hrs";
+    else return days + " Days"
+}
+
+const durationInMs = convertMilliSec(endTimenanosecond) - convertMilliSec(startTimenanosecond)
+const durationInMnt = (durationInMs / 60000)
+const durationInHrs = msToTime(durationInMs);
+
+console.log("startTimenanosecond", convertMilliSec(startTimenanosecond));
+console.log("endTimenanosecond", convertMilliSec(endTimenanosecond));
+console.log(durationInMs);
+console.log(durationInMnt);
+console.log(durationInHrs);
+console.log(userSteps);
+
+const stepsEl = document.getElementById('steps');
+const distanceEl = document.getElementById('distance');
+const caloriesEl = document.getElementById('calories');
+const durationEl = document.getElementById('duration');
+
+distanceEl ? (distanceEl.innerText = distanceInKm) : '';
+stepsEl ? (stepsEl.innerText = userSteps + ' kcal') : '';
+durationEl ? (durationEl.innerText = durationInHrs) : '';
+caloriesEl ? (caloriesEl.innerText = calories + ' kcal') : '';
